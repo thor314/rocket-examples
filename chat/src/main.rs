0425615a09d3b5f0329682f1e54d2916f5c6a7fb
thor <thorck@protonmail.com>
2022-07-20 12:29:43 -0700

    temp

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -4,15 +4,15 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(dead_code)]
-// use anyhow::Result;
-// use clap::Parser;
-// use futures::{
-//   channel::mpsc,
-//   executor::{self, ThreadPool},
-//   StreamExt,
-// };
-// use utils::MyError;
-// use validator::{Validate, ValidationError};
+use anyhow::Result;
+use clap::Parser;
+use futures::{
+  channel::mpsc,
+  executor::{self, ThreadPool},
+  StreamExt,
+};
+use utils::MyError;
+use validator::{Validate, ValidationError};
 
 mod utils;
 #[macro_use] extern crate rocket;
--- tk-protostreamer/src/main.rs
+++ tk-protostreamer/src/main.rs
@@ -34,7 +34,8 @@ use rocket::{
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
 #[serde(crate = "rocket::serde")]
-struct Message {
+struct BroadcastMessage {
+  pub from:     String,
   #[field(validate = len(..30))]
   pub room:     String,
   #[field(validate = len(..20))]
@@ -43,28 +44,30 @@ struct Message {
 }
 /// Returns an infinite stream of server-sent events. Each event is a message
 /// pulled from a broadcast queue sent by the `post` handler.
+/// See https://docs.rs/rocket/0.5.0-rc.2/rocket/response/stream/macro.stream.html
+/// Take: Any series of statements
+/// Expand into an `impl Stream<Item = T>`.
 #[get("/events")]
-async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+async fn events(queue: &State<Sender<BroadcastMessage>>, mut end: Shutdown) -> EventStream![] {
   let mut rx = queue.subscribe();
   EventStream! {
-      loop {
-          let msg = select! {
-              msg = rx.recv() => match msg {
-                  Ok(msg) => msg,
-                  Err(RecvError::Closed) => break,
-                  Err(RecvError::Lagged(_)) => continue,
-              },
-              _ = &mut end => break,
-          };
-
-          yield Event::json(&msg);
-      }
+    loop {
+      let msg = select! {
+        msg = rx.recv() => match msg {
+          Ok(msg) => msg,
+          Err(RecvError::Closed) => break,
+          Err(RecvError::Lagged(_)) => continue,
+        },
+        _ = &mut end => break,
+      };
+      yield Event::json(&msg); // Stream<Item = Event> // with data = BroadcastMessage
+    }
   }
 }
 
 /// Receive a message from a form submission and broadcast it to any receivers.
 #[post("/message", data = "<form>")]
-fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+fn post(form: Form<BroadcastMessage>, queue: &State<Sender<BroadcastMessage>>) {
   // A send 'fails' if there are no active subscribers. That's okay.
   let _res = queue.send(form.into_inner());
 }
@@ -72,7 +75,7 @@ fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
 #[launch]
 fn rocket() -> _ {
   rocket::build()
-    .manage(channel::<Message>(1024).0)
-    // .mount("/", routes![post, events])
+    .manage(channel::<BroadcastMessage>(1024).0)
+    .mount("/", routes![post, events])
     .mount("/", FileServer::from(relative!("static")))
 }
